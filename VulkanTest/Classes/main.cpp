68db6c3bfceaa2c5a719224d9c48c805d61fc068
DESKTOP-J612LV9 <sleicreider@outlook.com>
2022-06-05 02:09:02 +0200

    vulkan initial

---
+++ VulkanTest/Classes/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+//auto include vulkan
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "TestWindow", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    printf("Extension Count: %d", extensionCount);
+
+    glm::mat4 textMatrix(1.f);
+    glm::vec4 testVector(1.f);
+
+
+    auto testResult = textMatrix * testVector;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    //std::cout << "Hello World" << std::endl;
+    //return 0;
+}
